367c8cc7a1404fb9a3de235d27da849479ef2a6e
songhm <songhm@qssec.com>
2019-01-06 22:09:38 +0800

    为实例增加链接功能

--- boards/views.py
+++ boards/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 from .models import Board
 
@@ -13,5 +13,8 @@ def home(request):
     return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404 
     return render(request, 'topics.html', {'board': board})
