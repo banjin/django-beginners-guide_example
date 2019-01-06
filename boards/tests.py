17a1512b56d30fb3637adda95015b131a2c66732
songhm <songhm@qssec.com>
2019-01-06 21:30:12 +0800

    增加测试用例

--- boards/tests.py
+++ boards/tests.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.urls import resolve
+from .views import home
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
